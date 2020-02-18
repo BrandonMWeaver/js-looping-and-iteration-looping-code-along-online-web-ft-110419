713554aea80b872d9e6c33ee1d84ce83af5cea7a
Brandon Weaver <a7xncob@gmail.com>
2020-02-18 19:42:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`)
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    
   }
-  return gifts;
 }
