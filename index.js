7fc2c24c93af21fa2ba9af9be45762737df5d5cc
Brandon Weaver <a7xncob@gmail.com>
2020-02-18 19:43:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function writeCards(names) {
+  thankYouMessages = [];
   for (let i = 0; i < names.length; i++) {
-    
+    thankYouMessages.push(`Thank you, ${names[i]}!`)
   }
+  return thankYouMessages;
 }
